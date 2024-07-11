0ed758646a65ec09bc9c6cbadc364c0b363c4ee7
Rohitjakkam <rohitjakkam@gmail.com>
2024-07-12 04:45:55 +0530

    update pipeline stage Data Ingestion

--- src/mlProject/components/data_ingestion.py
+++ src/mlProject/components/data_ingestion.py
@@ -17,3 +17,13 @@ class DataIngestion:
                 filename= self.config.local_data_file
             )
             
+    def extract_zip_file(self):
+        """
+        zip_file_path: str
+        Extracts the zip file into the data directory
+        Function returns None
+        """
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
